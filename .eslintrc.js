09d441d4acbca7a26907db5a5d23dc317f29588e
Konstantin <stoyanov.k1992@gmail.com>
2020-07-17 14:47:22 +0300

    chage config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
   extends: [
     "airbnb",
-    "eslint:recommended",
+    "eslint",
     "plugin:import/errors",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended",
+    "plugin:react",
+    "plugin:jsx-a11y",
+    "plugin:prettier",
     "prettier/react",
   ],
   plugins: ["react-hooks"],
   rules: {
-    "prettier/prettier": ["warn"],
-    "react/jsx-filename-extension": ["warn", { extensions: [".js"] }],
+    "prettier/prettier": ["error"],
+    "react/jsx-filename-extension": ["warn", { extensions: [".js", ".jsx"] }],
     "react/jsx-props-no-spreading": ["off"],
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
   },
 };
